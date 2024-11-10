5df1c4eff439da1ba4471f0d49a8cc497f359164
Pallavi Kailas <119298061+pallavikailas@users.noreply.github.com>
2024-11-10 18:09:14 +0530

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,11 +1,17 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 def load_data(file_path):
     data = pd.read_csv(file_path)
     return data
 
 def preprocess_data(df):
-    # Preprocessing steps such as handling missing values, encoding, scaling, etc.
     df = df.dropna()
-    # Implement more preprocessing steps as needed
+    label_encoders = {}
+    
+    for column in df.select_dtypes(include=['object']).columns:
+        le = LabelEncoder()
+        df[column] = le.fit_transform(df[column])
+        label_encoders[column] = le
+
     return df
